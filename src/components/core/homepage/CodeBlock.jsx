9cdb0835c1c3630185e805710d3fb21df739ba31
Abhishek Kumar Singh <krabhi1452@gmail.com>
2026-07-12 23:56:26 +0530

    make responsive initial of codeblock and home page

--- src/components/core/homepage/CodeBlock.jsx
+++ src/components/core/homepage/CodeBlock.jsx
@@ -1,23 +1,24 @@
 
+
 import CTAButton from "./Button";
 import { FaArrowRight } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 
 export default function CodeBlock({position, heading, subheading, ctabtn1, ctabtn2, codeblock, bggradient, codecolor}){
     return(
-        <div className= {`flex ${position} gap-16  w-[80%] justify-center items-center`}>
-            <div className=" w-[50%]">
+        <div className= {`flex flex-col ${position} gap-8 lg:gap-16 w-[90%] lg:w-[80%] justify-center items-center`}>
+            <div className=" w-full lg:w-[50%]">
                 {/* left text section */}
-                <h1 className=" text-3xl font-bold">
+                <h1 className=" text-2xl sm:text-3xl font-bold">
                     {heading}
                 </h1>
                 <p className=" text-richblack-300 text-left font-medium mt-5">
                     {subheading}
                 </p>
-                <div className=" flex flex-row gap-6 mt-14">
+                <div className=" flex flex-row gap-4 sm:gap-6 mt-8 lg:mt-14">
                 
                     <CTAButton active={ctabtn1.active} linkto={ctabtn1.linkto}>
-                        <div className=" flex flex-row gap-3 items-center justify-center">
+                        <div className=" flex flex-row gap-3 items-center justify-center text-sm sm:text-base">
                             {ctabtn1.btnText}
                             <FaArrowRight/>
                         </div>
@@ -28,12 +29,12 @@ export default function CodeBlock({position, heading, subheading, ctabtn1, ctabt
                 
                 </div>
             </div>
-            <div className=" flex flex-row w-[50%] p-1  rounded-md bg-bgcodegradient opacity-80 shadow-2xl text-left justify-start items-baseline relative">
+            <div className=" flex flex-row w-full lg:w-[50%] p-1 overflow-x-auto rounded-md bg-bgcodegradient opacity-80 shadow-2xl text-left justify-start items-baseline relative">
                 {/* right code setion */}
                 <div className={` w-[320px] h-[210px] rounded-full opacity-30 blur-2xl absolute top-10 left-10 ring-transparent ${bggradient}`}>  {/*for bg gradient*/}
 
                 </div> 
-                <div className=" flex flex-col gap-1 text-center font-medium font-mono text-richblack-400 w-[10%]  bg-opacity-50">
+                <div className=" flex flex-col gap-1 text-center font-medium font-mono text-richblack-400 w-[10%]  bg-opacity-50 text-xs sm:text-sm">
                     <div>1</div>
                     <div>2</div>
                     <div>3</div>
@@ -46,7 +47,7 @@ export default function CodeBlock({position, heading, subheading, ctabtn1, ctabt
                     <div>10</div>
                     <div>11</div>
                 </div>
-                    <div className={`font-mono w-[90%] flex flex-col gap-1 font-medium ${codecolor} h-fit bg-opacity-30`}>
+                    <div className={`font-mono w-[90%] flex flex-col gap-1 font-medium ${codecolor} h-fit bg-opacity-30 text-xs sm:text-sm`}>
                         <pre className=" whitespace-pre-wrap">
                             
                         <TypeAnimation
