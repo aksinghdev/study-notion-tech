db474da732d49d2a68e59bfd8b5a893244c5beda
Abhishek Kumar Singh <krabhi1452@gmail.com>
2026-07-13 00:18:10 +0530

    make responsive of template.jsx

--- src/components/core/auth/Template.jsx
+++ src/components/core/auth/Template.jsx
@@ -1,4 +1,49 @@
 
+// import LoginForm from "./LoginForm";
+// import imgFrame from "../../../assets/Images/frame.png"
+// import SignupForm from "./SignupForm";
+
+// export default function Template ({
+//   heading,
+//   description1,
+//   image,
+//   formType,
+// }) {
+
+//     var loading = false;
+
+//     return (
+//         <div>
+//         {loading ? (
+//             <div className=" spinner"></div>
+//         ) : (
+//             <div className=" flex flex-row gap-20 justify-between items-center mx-auto max-w-maxContent pb-14  w-11/12 ">
+//                 {/* text and form section */}
+//                 <div className=" flex flex-col gap-5 items-start justify-center w-full max-w-[36%] pt-32">
+//                     <div className=" flex flex-col gap-5 items-start ">
+//                     <h1 className=" font-inter font-semibold text-3xl text-start">{heading}</h1>
+//                     <p>{description1}</p>
+//                     </div>
+//                     <div className=" w-full items-start">
+//                         {
+//                             formType === "login" ? (<LoginForm/>): (<SignupForm/>)
+//                         }
+//                     </div>
+//                 </div>
+//                 {/* image section */}
+//                 <div className=" relative mt-40">    
+//                     <img src={imgFrame}/>
+//                     <img src={image} 
+//                         className=" absolute bottom-7 right-7"
+//                     />
+//                 </div>
+
+//             </div>
+//         )}
+//         </div>
+//     );
+// };
+
 import LoginForm from "./LoginForm";
 import imgFrame from "../../../assets/Images/frame.png"
 import SignupForm from "./SignupForm";
@@ -17,11 +62,11 @@ export default function Template ({
         {loading ? (
             <div className=" spinner"></div>
         ) : (
-            <div className=" flex flex-row gap-20 justify-between items-center mx-auto max-w-maxContent pb-14  w-11/12 ">
+            <div className=" flex flex-col lg:flex-row gap-10 lg:gap-20 justify-between items-center mx-auto max-w-maxContent pb-14  w-11/12 ">
                 {/* text and form section */}
-                <div className=" flex flex-col gap-5 items-start justify-center w-full max-w-[36%] pt-32">
+                <div className=" flex flex-col gap-5 items-start justify-center w-full lg:max-w-[36%] pt-8 lg:pt-32">
                     <div className=" flex flex-col gap-5 items-start ">
-                    <h1 className=" font-inter font-semibold text-3xl text-start">{heading}</h1>
+                    <h1 className=" font-inter font-semibold text-2xl sm:text-3xl text-start">{heading}</h1>
                     <p>{description1}</p>
                     </div>
                     <div className=" w-full items-start">
@@ -30,8 +75,8 @@ export default function Template ({
                         }
                     </div>
                 </div>
-                {/* image section */}
-                <div className=" relative mt-40">    
+                {/* image section - hidden on mobile/tablet, shown on lg+ */}
+                <div className=" hidden lg:block relative mt-40">    
                     <img src={imgFrame}/>
                     <img src={image} 
                         className=" absolute bottom-7 right-7"
