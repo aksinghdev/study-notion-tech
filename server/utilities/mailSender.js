47045c97c9a460ed52e357f8f427af8b00e65ff3
Abhishek Kumar Singh <krabhi1452@gmail.com>
2026-07-14 17:55:14 +0530

    replace mailsender file , nodemailer to brevo

--- server/utilities/mailSender.js
+++ server/utilities/mailSender.js
@@ -1,25 +1,59 @@
-const nodemailer = require("nodemailer");
+
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
+// const mailSender = async (email, title, body ) =>{
+//     console.log("mailsender triger outside try block")
+//     try{
+//         console.log("mailsender inside try block")
+//         const transporter = nodemailer.createTransport({
+//             host : process.env.MAIL_HOST ,
+//             auth : {
+//                 user : process.env.MAIL_USER ,
+//                 pass : process.env.MAIL_PASS,        
+//             }
+//         })
+
+//         let info = await transporter.sendMail({
+//             from : 'STUDY NOTION INDIA',
+//             to : `${email}`,
+//             subject : `${title}`,
+//             html : `${body}`,
+//         })
+//         console.log("Sent mail info",info);
+//         return info;
+
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+// }
+// module.exports = mailSender;
+
+
+
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 require("dotenv").config();
 
-const mailSender = async (email, title, body ) =>{
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+const mailSender = async (email, title, body) => {
     console.log("mailsender triger outside try block")
     try{
         console.log("mailsender inside try block")
-        const transporter = nodemailer.createTransport({
-            host : process.env.MAIL_HOST ,
-            auth : {
-                user : process.env.MAIL_USER ,
-                pass : process.env.MAIL_PASS,        
-            }
-        })
 
-        let info = await transporter.sendMail({
-            from : 'STUDY NOTION INDIA',
-            to : `${email}`,
-            subject : `${title}`,
-            html : `${body}`,
-        })
-        console.log("Sent mail info",info);
+        const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+        const sendSmtpEmail = {
+            sender: { email: "krabhishek.8828@gmail.com", name: "STUDY NOTION INDIA" },
+            to: [{ email: `${email}` }],
+            subject: `${title}`,
+            htmlContent: `${body}`,
+        };
+
+        let info = await apiInstance.sendTransacEmail(sendSmtpEmail);
+        console.log("Sent mail info", info);
         return info;
 
     }
